3fd797bf6617bde639fe518fce6b736b44a11996
Akash <akashmondkar0303@gmail.com>
2026-01-01 21:20:51 +0530

    navbar and footer components to the main application layout

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,17 @@
-
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Layout/Navbar.jsx'
+import Footer from './components/Layout/Footer.jsx'
 
 
 function App() {
 
 
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+     <Navbar />
+     <Footer/>
+    </BrowserRouter>
   )
 }
 
