9d648a97a66f78573f631761e2cf6165a330b2ea
Akash <akashmondkar0303@gmail.com>
2026-01-03 17:58:01 +0530

    shortlog: Update Navbar styles and link colors

---
+++ my-app/src/components/Layout/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-10">
+      © 2026 ShopKart. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
--- my-app/src/components/Layout/Navbar.jsx
+++ my-app/src/components/Layout/Navbar.jsx
@@ -1,20 +1,34 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link> 
-        </li>   
-        <li>
-            <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+    <nav className="bg-indigo-600 text-white">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center h-14">
+          
+          {/* Logo */}
+          <Link to="/" className="font-bold text-xl">
+            ShopKart
+          </Link>
+
+          {/* Menu */}
+          <div className="flex space-x-6">
+            <Link to="/" className="hover:text-gray-200">
+              Home
+            </Link>
+            <Link to="/shop" className="hover:text-gray-200">
+              Shop
+            </Link>
+            <Link to="/cart" className="hover:text-gray-200">
+              Cart
+            </Link>
+            <Link to="/login" className="hover:text-gray-200">
+              Login
+            </Link>
+          </div>
+
+        </div>
+      </div>
     </nav>
   );
 };
