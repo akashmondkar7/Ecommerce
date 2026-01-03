36b2c84c5d7a63991c0f4737d62d66e691f06852
Akash <akashmondkar0303@gmail.com>
2026-01-03 18:22:36 +0530

    2

--- my-app/src/components/Layout/Navbar.jsx
+++ my-app/src/components/Layout/Navbar.jsx
@@ -2,27 +2,27 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-indigo-600 text-white">
+    <nav className="white  shadow-md shadow-gray-300 bg-white">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center h-14">
           
           {/* Logo */}
-          <Link to="/" className="font-bold text-xl">
+          <Link to="/" className="text-orange-500 font-bold text-xl">
             ShopKart
           </Link>
 
           {/* Menu */}
           <div className="flex space-x-6">
-            <Link to="/" className="hover:text-gray-200">
+            <Link to="/" className="text-zinc-400  hover:text-orange-400">
               Home
             </Link>
-            <Link to="/shop" className="hover:text-gray-200">
+            <Link to="/shop" className="text-zinc-400 hover:text-orange-400">
               Shop
             </Link>
-            <Link to="/cart" className="hover:text-gray-200">
+            <Link to="/cart" className="text-zinc-400 hover:text-orange-400">
               Cart
             </Link>
-            <Link to="/login" className="hover:text-gray-200">
+            <Link to="/login" className="text-zinc-400 hover:text-orange-400">
               Login
             </Link>
           </div>
